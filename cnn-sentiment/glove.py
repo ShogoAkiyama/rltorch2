87a40cab0d40b911c06f2a3b75671d0c0c1e0f0e
ShogoAkiyama <19nm401h@vc.ibaraki.ac.jp>
2020-01-11 11:08:49 +0900

    fix

---
+++ cnn-sentiment/glove.py
@@ -0,0 +1,105 @@
+import torch
+from torchtext import data
+from torchtext import datasets
+import random
+import numpy as np
+import time
+import torch.nn as nn
+
+from model import CNN
+from utils import *
+
+# パラメータの設定
+SEED = 1234
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+# テキストデータ
+TEXT = data.Field(tokenize='spacy', batch_first=True)
+LABEL = data.LabelField(dtype=torch.float)
+
+train_data, test_data = datasets.IMDB.splits(TEXT, LABEL)
+
+train_data, valid_data = train_data.split(random_state=random.seed(SEED))
+
+MAX_VOCAB_SIZE = 25_000
+
+# データの準備
+TEXT.build_vocab(train_data,
+                 max_size = MAX_VOCAB_SIZE,
+                 vectors = "glove.6B.100d",
+                 unk_init = torch.Tensor.normal_)
+
+LABEL.build_vocab(train_data)
+
+BATCH_SIZE = 64
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
+    (train_data, valid_data, test_data),
+    batch_size = BATCH_SIZE,
+    device = device)
+
+
+INPUT_DIM = len(TEXT.vocab)
+EMBEDDING_DIM = 100
+N_FILTERS = 100
+FILTER_SIZES = [3,4,5]
+OUTPUT_DIM = 1
+DROPOUT = 0.5
+PAD_IDX = TEXT.vocab.stoi[TEXT.pad_token]
+
+model = CNN(INPUT_DIM, EMBEDDING_DIM, N_FILTERS, FILTER_SIZES, OUTPUT_DIM, DROPOUT, PAD_IDX)
+
+print(f'The model has {count_parameters(model):,} trainable parameters')
+
+pretrained_embeddings = TEXT.vocab.vectors
+
+model.embedding.weight.data.copy_(pretrained_embeddings)
+
+UNK_IDX = TEXT.vocab.stoi[TEXT.unk_token]
+
+model.embedding.weight.data[UNK_IDX] = torch.zeros(EMBEDDING_DIM)
+model.embedding.weight.data[PAD_IDX] = torch.zeros(EMBEDDING_DIM)
+
+import torch.optim as optim
+
+optimizer = optim.Adam(model.parameters())
+
+criterion = nn.BCEWithLogitsLoss()
+
+model = model.to(device)
+criterion = criterion.to(device)
+
+
+N_EPOCHS = 5
+
+best_valid_loss = float('inf')
+
+for epoch in range(N_EPOCHS):
+
+    start_time = time.time()
+
+    train_loss, train_acc = train(model, train_iterator, optimizer, criterion)
+    valid_loss, valid_acc = evaluate(model, valid_iterator, criterion)
+
+    end_time = time.time()
+
+    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'tut4-model.pt')
+
+    print(f'Epoch: {epoch + 1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
+    print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc * 100:.2f}%')
+    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc * 100:.2f}%')
+
+model.load_state_dict(torch.load('tut4-model.pt'))
+
+test_loss, test_acc = evaluate(model, test_iterator, criterion)
+
+print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc*100:.2f}%')
