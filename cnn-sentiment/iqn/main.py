b0fdf7f503c4ed5668f0ff500a5375d19ebd6f58
ShogoAkiyama <19nm401h@vc.ibaraki.ac.jp>
2020-01-24 14:40:26 +0900

    fix

---
+++ cnn-sentiment/iqn/main.py
@@ -0,0 +1,73 @@
+import os
+import argparse
+import shutil
+
+import torchtext
+import torch
+
+from utils import tokenizer_with_preprocessing
+from trainer import Trainer
+
+NEWS_PATH = os.path.join('..', 'data', 'news')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--max_length', type=int, default=256)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--n_epochs', type=int, default=100)
+    parser.add_argument('--target_update_freq', type=int, default=100)
+    parser.add_argument('--evaluation_freq', type=int, default=10)
+    parser.add_argument('--network_save_freq', type=int, default=100)
+    parser.add_argument('--num_actions', type=int, default=1)
+
+    parser.add_argument('--min_freq', type=int, default=10)
+    parser.add_argument('--embedding_dim', type=int, default=300)
+    parser.add_argument('--n_filters', type=int, default=100)
+    parser.add_argument('--filter_sizes', type=list, default=[3, 4, 5])
+    parser.add_argument('--pad_idx', type=list, default=1)
+    parser.add_argument('--gamma', type=float, default=0.99)
+    parser.add_argument('--learning_rate', type=float, default=2.5e-5)
+
+    parser.add_argument('--test', action='store_true')
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    parser.add_argument('--device', type=str, default=device)
+
+    # IQN
+    parser.add_argument('--num_quantile', type=int, default=64)
+
+    args = parser.parse_args()
+
+    torch.backends.cudnn.benchmark = True
+
+    # ログファイルを入れる
+    shutil.rmtree('./logs')
+    os.mkdir('./logs')
+    
+
+    # 読み込んだ内容に対して行う処理を定義
+    TEXT = torchtext.data.Field(sequential=True, tokenize=tokenizer_with_preprocessing,
+                                use_vocab=True, lower=True, include_lengths=True,
+                                batch_first=True, fix_length=args.max_length,
+                                init_token="<cls>", eos_token="<eos>")
+    LABEL = torchtext.data.Field(sequential=False, use_vocab=False, dtype=torch.float)
+
+    train_ds = torchtext.data.TabularDataset.splits(
+        path=NEWS_PATH, train='text_train.tsv',
+        format='tsv',
+        fields=[('Text1', TEXT), ('Text2', TEXT), ('Label', LABEL)])
+    train_ds = train_ds[0]
+
+    TEXT.build_vocab(train_ds, min_freq=args.min_freq)
+    TEXT.vocab.freqs
+
+    train_dl = torchtext.data.Iterator(
+        train_ds, batch_size=args.batch_size, train=True)
+
+    trainer = Trainer(args, TEXT, train_dl)
+
+    if args.test:
+        trainer.test()
+    else:
+        trainer.run()
+
---
+++ cnn-sentiment/iqn/trainer.py
@@ -0,0 +1,140 @@
+import os
+import numpy as np
+from glob import glob
+
+import torch.optim as optim
+import torch
+
+from model import IQN
+
+LOG_DIR = os.path.join('.', 'logs')
+
+
+class Trainer:
+    def __init__(self, args, TEXT, train_dl):
+
+        self.train_dl = train_dl
+
+        self.target_update_freq = args.target_update_freq
+        self.evaluation_freq = args.evaluation_freq
+        self.network_save_freq = args.network_save_freq
+
+        self.device = args.device
+        self.gamma = args.gamma
+        self.num_actions = args.num_actions
+
+        # quantile
+        self.num_quantile = args.num_quantile
+
+        self.epochs = 0
+        self.epoch_loss = 0
+
+        vocab_size = len(TEXT.vocab.freqs)
+        self.model = IQN(vocab_size, args.embedding_dim, args.n_filters,
+                           args.filter_sizes, args.pad_idx, args.num_actions).to(args.device)
+        self.target_model = IQN(vocab_size, args.embedding_dim, args.n_filters,
+                                args.filter_sizes, args.pad_idx, args.num_actions).to(args.device)
+
+        self.target_model.load_state_dict(self.model.state_dict())
+
+        self.optimizer = optim.Adam(self.model.parameters(), lr=args.learning_rate)
+
+    def run(self):
+        while True:
+            self.epoch_loss = 0
+            self.epochs += 1
+            self.train_episode()
+
+            # update target_model
+            if self.epochs % self.target_update_freq == 0:
+                self.target_model.load_state_dict(self.model.state_dict())
+
+            if self.epochs % self.evaluation_freq == 0:
+                self.evaluation(self.train_dl)
+
+            if self.epochs % self.network_save_freq == 0:
+                self.save_model()
+
+            self.log()
+
+    def train_episode(self):
+        for batch in self.train_dl:
+            states = batch.Text1[0].to(self.device)
+            next_states = batch.Text2[0].to(self.device)
+            rewards = batch.Label.to(self.device)
+
+            self.train(states, next_states, rewards)
+
+    def train(self, states, next_states, rewards):
+        curr_q, tau = self.model(states)
+
+        # target_q
+        with torch.no_grad():
+            next_q, _ = self.target_model(next_states)
+            next_q = next_q.squeeze(2)
+
+            target_q = rewards.reshape(-1, 1) + self.gamma * next_q
+            target_q = target_q.unsqueeze(2)
+            target_q = target_q.repeat(1, 1, self.num_quantile)
+            target_q = target_q.permute(0, 2, 1)
+
+        diff = target_q.t().unsqueeze(-1) - curr_q.unsqueeze(0)
+
+        loss = self.huber(diff)
+
+        I_delta = (diff<0).double()
+        loss *= torch.abs(tau - I_delta)
+
+        # huber loss
+        loss = torch.mean(torch.sum(torch.mean(loss, dim=2), dim=1))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        for param in self.model.parameters():
+            param.grad.data.clamp_(-1, 1)
+        self.optimizer.step()
+
+        self.epoch_loss += loss.item()
+
+    def evaluation(self, dl):
+        epi_rewards = 0
+        dist_hist = []
+        rewards_hist = []
+
+        for batch in dl:
+            states = batch.Text1[0].to(self.device)
+            rewards = batch.Label.to(self.device)
+
+            with torch.no_grad():
+                dist, _ = self.model(states)
+                dist_hist.append(dist.cpu().detach().numpy())
+                rewards_hist.append(rewards.cpu().detach().numpy())
+                _mean = dist.sum(dim=2)
+                actions = torch.where(
+                            _mean > 0,
+                            torch.LongTensor([1]).to(self.device),
+                            torch.LongTensor([0]).to(self.device))
+
+            epi_rewards += (actions * rewards).detach().cpu().numpy().sum()
+
+        print(' '*20,
+              'train_reward: ', epi_rewards)
+        
+        return dist_hist, rewards_hist
+
+    def huber(self, x):
+        cond = (x.abs() < 1.0).float().detach()
+        return 0.5 * x.pow(2) * cond + (x.abs() - 0.5) * (1.0 - cond)
+
+    def log(self):
+        print('epoch: ', self.epochs,
+              ' loss: {:.3f}'.format(self.epoch_loss))
+
+    def save_model(self):
+        torch.save(self.model.state_dict(), os.path.join(LOG_DIR, str(self.epochs))+'.pt')
+
+    def load_model(self):
+        model_path = sorted(glob(os.path.join(LOG_DIR, '*')))[-1]
+        self.model.load_state_dict(torch.load(model_path))
+    
